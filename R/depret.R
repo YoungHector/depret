ca674177d7a3957225fb8d8bb06b04e1f524c53a
younghector <hectoryang1995@gmail.com>
2019-04-25 09:58:39 +0800

    add S3 class compatibility

--- R/depret.R
+++ R/depret.R
@@ -1,10 +1,4 @@
 #   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
 
 # Use R code to connect deployed machine-learning model or api.
 # depret -- Deployed-model interpretion.
